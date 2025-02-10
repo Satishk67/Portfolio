0e4f3a36193ffdb23120b13280653a3467cde4d6
Satish Kumar <satishsinha98k@gmail.com>
2025-02-10 22:50:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -12,6 +12,24 @@ window.onload = function () {
     window.location.href = "#";
 };
 
+document.addEventListener("DOMContentLoaded", function () {
+    const me = document.querySelector(".me");
+  
+    me.addEventListener("mousemove", (e) => {
+      const rect = me.getBoundingClientRect();
+      const x = (e.clientX - rect.left) / rect.width - 0.5; 
+      const y = (e.clientY - rect.top) / rect.height - 0.5; 
+      const rotateX = y * 30; 
+      const rotateY = x * -30; 
+  
+      me.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)  `;
+    });
+  
+    me.addEventListener("mouseleave", () => {
+      me.style.transform = "rotateX(0deg) rotateY(0deg)"; // Reset on leave
+    });
+  });
+  
 
 
 
